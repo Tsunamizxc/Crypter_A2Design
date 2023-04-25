1875ea920331361c9af801f4f165ca741605e234
Tsunamizxc <semen.gnedenkov@mail.ru>
2023-04-25 18:37:41 +0600

    до collection finish

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ $('.sellers__slider').slick({
     autoplay: true,
     autoplaySpeed: 2000,
 });
+
+
+$('.collection__for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.collection__nav'
+});
+$('.collection__nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.collection__for',
+
+});
